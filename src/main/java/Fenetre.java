b1c357022b29fe245264b8c27176ae57e65ef55d
rodolphe <rodolphe.riviere@hotmail.fr>
2018-09-11 11:49:20 +0200

    second commit

--- src/main/java/Fenetre.java
+++ src/main/java/Fenetre.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Dimension;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,92 +9,60 @@ import javax.swing.JPanel;
 public class Fenetre extends JFrame{
 	private Panneau pan = new Panneau();
 	private String Text = "";
-	
-	
+
+
 	//initialiser les valeurs du jeu
-	private int size =200; //taille du monde
-	//int cell = 3; //nbr de cell de départ   
-	private char tab[][] = new char[this.size][this.size];	//tableau du monde
-	private char newtab[][] = new char[this.size][this.size]; //tableau du nouveau monde
-	
+	private int size = pan.getSiz(); //taille du monde  
+	private boolean tab[][] = new boolean[this.size][this.size];	//tableau du monde
+	private boolean newtab[][] = new boolean[this.size][this.size]; //tableau du nouveau monde
+	private boolean flag = true ;
+
 	public Fenetre(){        
 		this.setTitle("It's alive !!!");
-		this.setSize(1000, 1000);
-		//this.pack();
+		this.setSize(size, size);
+		//this.setPreferredSize(new Dimension(500,500));
+		
+		this.pack();
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		this.setContentPane(pan);
 		this.setVisible(true);
-		
-		//positions de départ
-//		this.tab[2][1] = 'X';
-//		this.tab[2][2] = 'X';
-//		this.tab[2][3] = 'X';
-		
+
+
 		Random rnd = new Random();
 		for (int i = 0; i<size; i++)
 		{
 			for (int j=0; j<size; j++)
 			{
-				if (rnd.nextBoolean())
+				if (rnd.nextBoolean() && rnd.nextBoolean() && rnd.nextBoolean() )
 				{
-					this.tab[j][i]='X' ;
+					this.tab[j][i]=true ;
 				}
 				else
 				{
-					this.tab[j][i]=' ' ;
+					this.tab[j][i]=false ;
 				}
 			}
 		}
-		
+
 		//lancer la fenetre avec le jeu
 		go();
 	}
 
 	private void go(){
 		for(;;){
-			int x = pan.getPosX(), y = pan.getPosY();
-			//x++;
-			//y++;
-			x = this.getWidth()/4;
-			y = this.getHeight()/4;
-			pan.setPosX(x);
-			pan.setPosY(y);
-			//afficher la condition de départ dans la console
-			this.Text = this.Tab2String(tab);
-			//String essai = Character.to
-			//System.out.println(Text);
+			int x = pan.getPosX();
+			int y = pan.getPosY();
+//			pan.setPosX(x);
+//			pan.setPosY(y);
 
-			//calcul du nouveau monde;
-			for (int i = 0; i<this.tab.length; i++)
+			if (flag)
 			{
-				for (int j=0; j<this.tab.length; j++)
-				{
-					if(this.isAlive(this.tab, j, i))
-					{
-						newtab[j][i]='X';
-					}else
-					{
-						newtab[j][i]=' ';
-					}
-				}
+				pan.setTab(this.tab);
+				flag = false;
 			}
-			//on récupere le monde dans un texte
-			Text = Tab2String(newtab);
-			pan.setAlive(newtab);
-			// on met à jour la table pour continuer la simulation
-			for (int i = 0; i<size; i++)
-			{
-				for (int j=0; j<size; j++)
-				{
-					tab[i][j]=newtab[i][j];
-				}
-			}
-			
 			//on affiche dans la fenetre
-								
-			//pan.setText(this.Text);
-			
+
 			pan.repaint();  
 //			try {
 //				Thread.sleep(10);
@@ -102,50 +71,13 @@ public class Fenetre extends JFrame{
 //			}
 		}
 	} 
-	public void setText(String Text) {
-		this.Text = Text;
-	}
+
 	//////////////////////////////// POUR LE JEU DE LA VIE //////////////////////////////////
-
-	//	Methode pour afficher un tableau 2 dimensions 
-	public static void displayTab2 (char Tab[][]) {
-
-
-		for (int i = 0; i<Tab.length; i++)
-		{
-			for (int j=0; j<Tab.length; j++)
-			{
-				System.out.print("["+Tab[i][j]+"]" );
-				//System.out.print(Tab[i][j]);
-			}
-			System.out.print("\n" );
-		}
-
-	}
-	//	//	Methode pour copier le tableau dans un string
-	public static String Tab2String (char Tab[][]) {
-
-		String Text ="";
-
-		for (int i = 0; i<Tab.length; i++)
-		{
-			for (int j=0; j<Tab.length; j++)
-			{
-				//Text.concat("[").concat(Character.toString(Tab[i][j])).concat("]");
-				//Text = Text+"["+Character.toString(Tab[i][j])+"]";
-				Text = Text+Character.toString(Tab[i][j]);
-				//Text = Text+" "+Character.toString(Tab[i][j])+" ";
-			}
-			//System.out.print("\n" );
-			Text = Text+"\n";
-		}
-		return Text;
-
-	}
+	
 	//	Methode pour tester des cellules adjacente 
-	public static boolean isAlive (char Tab[][], int y, int x) {
+	public static boolean isAlive (boolean Tab[][], int y, int x) {
 
-		boolean isAlive = false;
+		boolean isAlive = Tab[y][x];
 		int counter = 0;
 		//on définit la zone à inspecter dans un carré dont les coordonnées sont
 		int x1 = x-1;
@@ -172,18 +104,12 @@ public class Fenetre extends JFrame{
 			y2=Tab.length-1;
 		}
 
-		//la cellule considérée est-elle vivante ?
-		if (Tab[y][x]=='X')
-		{
-			isAlive = true;
-		}
-
 		// y a -t-il des cellules vivantes autour
 		for (int i = y1; i<=y2; i++ )
 		{
 			for (int j = x1; j<=x2; j++)
 			{
-				if (Tab[i][j]=='X')
+				if (Tab[i][j])
 				{
 					counter++;
 				}
--- src/main/java/Panneau.java
+++ src/main/java/Panneau.java
@@ -7,76 +7,53 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Panneau extends JPanel {
-	private int posX = this.getWidth()/2;
-	private int posY = this.getHeight()/2;
+//	private int posX = this.getWidth()/2;
+//	private int posY = this.getHeight()/2;
+	private int posX = 0;
+	private int posY = 0;
 	private String Text  = "";
-	private int size = 200;
-	private int Alive[][]= new int[size][size];
-
+	private int size = 1000;
+	private boolean tab[][]= new boolean[size][size];
+	private boolean newtab[][]= new boolean[size][size];
 
 	public void paintComponent(Graphics g){
+		
 		//On choisit une couleur de fond pour le rectangle
 		g.setColor(Color.white);
 		//On le dessine de sorte qu'il occupe toute la surface
 		g.fillRect(0, 0, this.getWidth(), this.getHeight());
-		
-		//On le dessine aux coordonnées souhaitées
-		for (int i =0; i<this.Alive.length;i++)
+
+		//calcul du nouveau monde;
+		for (int i = 0; i<size; i++)
 		{
-			for (int j = 0; j<this.Alive.length;j++)
+			for (int j=0; j<size; j++)
 			{
-				if (this.Alive[i][j]==1)
+				if(Fenetre.isAlive(this.tab, j, i))
 				{
-					//On redéfinit une couleur pour le rond
-					//on génére une couleur aleatoire
-//					Random rnd = new Random();
-//					Color couleur = new Color (rnd.nextFloat(),rnd.nextFloat(),rnd.nextFloat());
-//					g.setColor(couleur);
+					newtab[j][i]=true;
 					g.setColor(Color.RED);
 					g.fillOval(i*this.getWidth()/this.size, j*this.getHeight()/this.size, this.getWidth()/this.size+4, this.getHeight()/this.size+4);
+	//				g.fillOval(i, j, this.getWidth()/this.size+4, this.getHeight()/this.size+4);
+
 				}else
 				{
-//					g.setColor(Color.WHITE);
-//					//g.fillOval(i*4, j*4, 5, 5);
-//					g.fillOval(i*this.getWidth()/this.size, j*this.getHeight()/this.size, this.getWidth()/this.size+5, this.getHeight()/this.size+5);
-//					
+					newtab[j][i]=false;
 				}
 			}
-			
 		}
-		
-		
-		//on affiche un texte
-			//		
-//		for (int i = 1; i<=5;i++)
-//		{
-//		g.drawString(this.Text.substring(0, 15), posX, posY);
-//		g.drawString(this.Text.substring(16, 31), posX, posY+12);
-//		g.drawString(this.Text.substring(32, 47), posX, posY+24);
-//		g.drawString(this.Text.substring(48, 63), posX, posY+36);
-//		g.drawString(this.Text.substring(64, 79), posX, posY+48);
-//		}
-		
-	}
-	public void setAlive(char tab[][]) {
-		//this.Alive = new int[tab.length][tab.length];
-		for (int i = 0; i<tab.length; i++)
+
+		// on met à jour la table pour continuer la simulation
+		for (int i = 0; i<size; i++)
 		{
-			for (int j=0; j<tab.length; j++)
+			for (int j=0; j<size; j++)
 			{
-				if (tab[i][j]=='X')
-				{
-					this.Alive[i][j]=1;
-				}else
-				{
-					this.Alive[i][j]=0;
-				}
+				tab[i][j]=newtab[i][j];
 			}
 		}
 	}
-//	public void setColor(Color couleur) {
-//		this.couleur = couleur;
-//	}
+	public int getSiz() {
+		return size;
+	}
 	public void setText(String Text) {
 		this.Text = Text;
 	}
@@ -95,5 +72,14 @@ public class Panneau extends JPanel {
 
 	public void setPosY(int posY) {
 		this.posY = posY;
-	}        
+	} 
+	public void setTab(boolean extab[][]) {
+		for (int i = 0; i<size; i++)
+		{
+			for(int j=0; j< size; j++)
+			{
+				this.tab[j][i]=extab[j][i];
+			}
+		}
+	}
 }
